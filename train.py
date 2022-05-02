a0f7b96b93cdb75d019cba7af9ca40fad9433e70
khalil-asli <56821845+khalil-asli@users.noreply.github.com>
2022-05-02 16:00:08 +0200

    Update train.py

--- train.py
+++ train.py
@@ -2,11 +2,7 @@ from sklearn.linear_model import LinearRegression
 
 predictor = LinearRegression(n_jobs=-1)
 predictor.fit(X=TRAIN_INPUT, y=TRAIN_OUTPUT)
-X_TEST = [[10, 20, 30]]
-outcome = predictor.predict(X=X_TEST)
-coefficients = predictor.coef_
 
-print('Outcome : {}\nCoefficients : {}'.format(outcome, coefficients))
 with open('input.data.json') as f:
   content=f.read()
   TRAIN_INPUT=loads(content)
