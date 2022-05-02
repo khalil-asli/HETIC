a4e9923ec87f793acf2c1a2846fe05cb3582158f
khalil-asli <56821845+khalil-asli@users.noreply.github.com>
2022-05-02 16:06:19 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,13 +1,15 @@
 from sklearn.linear_model import LinearRegression
 from json import dumps
 
-predictor = LinearRegression(n_jobs=-1)
-predictor.fit(X=TRAIN_INPUT, y=TRAIN_OUTPUT)
-print(predictor.coef_)
-
 with open('input.data.json') as f:
   content=f.read()
   TRAIN_INPUT=loads(content)
 with open('output.data.json') as f:
   content=f.read()
   TRAIN_OUTPUT=loads(content)
+
+predictor = LinearRegression(n_jobs=-1)
+predictor.fit(X=TRAIN_INPUT, y=TRAIN_OUTPUT)
+
+with open('model.json','w') as f:
+  f.write(dumps(predictor.coef_.tolist())
